c6aeec33913a7011b55d4803292a5e392d02ba3e
Gino <ginoalodar@gmail.com>
2022-04-30 19:12:32 -0500

    added inspire function for random inpiring quotes

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import discord
 import os
+import requests
+import json
 
 client = discord.Client()
+
+
+# random quotes with zenquotes.io
+def get_quote():
+    response = requests.get('https://zenquotes.io/api/random')
+    json_data = json.loads(response.text)
+    quote = json_data[0]['q'] + ' -' + json_data[0]['a']
+    return (quote)
 
 
 @client.event
@@ -17,6 +27,9 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello')
 
+    if message.content.startswith('$inspire'):
+        quote = get_quote()
+        await message.channel.send(quote)
+
 
 client.run(os.getenv('TOKEN'))
-
