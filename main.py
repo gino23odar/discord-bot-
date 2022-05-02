4000bab52390cb282054437683272f816ce92d3f
Gino <ginoalodar@gmail.com>
2022-05-01 19:25:33 -0500

    added a depression fighting feature to the bot

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ import discord
 import os
 import requests
 import json
+import random
 
 client = discord.Client()
+
+sad_list = ['depressed', 'depressing', 'melancholic', 'feeling blue', 'saddened', 'miserable']
+
+encouragement_words = ["Don't mind", "At least I like you.", "You are nice...kinda", "Want a cookie?"]
 
 
 # random quotes with zenquotes.io
@@ -21,15 +26,20 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    msg = message.content
+
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
+    if msg.startswith('$hello'):
         await message.channel.send('Hello')
 
-    if message.content.startswith('$inspire'):
+    if msg.startswith('$inspire'):
         quote = get_quote()
         await message.channel.send(quote)
+
+    if any(word in msg for word in sad_list):
+        await message.channel.send(random.choice(encouragement_words))
 
 
 client.run(os.getenv('TOKEN'))
